e34e66033f2eaf5be7ba559542be7ea475b01fe3
Tejas12malviya <91682736+Tejas12malviya@users.noreply.github.com>
2024-12-07 16:02:07 +0530

    Update ticket.py

--- ticket.py
+++ ticket.py
@@ -14,9 +14,9 @@ class train :
         print(f"Your train with Train no. {self.trainNO} is running on Time.")
 
     def Fair(self,fro,to):
-        print(f"Ticket fair of Train no. {self.trainNO} going from {fro} to {to} general coach is rupees {randint(100,300)}")
-        print(f"Ticket fair of Train no. {self.trainNO} going from {fro} to {to} sleeper coach is rupees {randint(500,900)}")
-        print(f"Ticket fair of Train no. {self.trainNO} going from {fro} to {to} A.C. coach is rupees {randint(1200,2000)}")
+        print(f"Ticket fair of Train no. {self.trainNO} going from {fro} to {to} in general coach is rupees {randint(100,300)}")
+        print(f"Ticket fair of Train no. {self.trainNO} going from {fro} to {to} in sleeper coach is rupees {randint(500,900)}")
+        print(f"Ticket fair of Train no. {self.trainNO} going from {fro} to {to} in A.C. coach is rupees {randint(1200,2000)}")
 
 t=train(12599)
 t.book("Nagpur","Hyderabad")
