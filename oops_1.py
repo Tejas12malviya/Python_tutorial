9e9a6aac4dc1fe64d356e9ac5328c4713f6629e8
Tejas12malviya <91682736+Tejas12malviya@users.noreply.github.com>
2024-12-09 22:28:15 +0530

    Add files via upload

---
+++ oops_1.py
@@ -0,0 +1,25 @@
+# Creating class
+class Student:
+    name= "Rohan kumar"
+
+# Creating object(instance)
+s1=Student()
+print(s1.name)
+
+# Constructor
+# It is a special function which is invoke while object creation.
+
+class Stud:
+    #  default constructor
+    def __init__(self):
+        pass
+
+    # parameterised constructor
+    def __init__(self,fname,marks):# data stored inside the class or inside the function is called attributes.
+        self.fname=fname
+        self.marks=marks
+        print(f"My name is {fname} and I got {marks} marks.")
+
+Stud("Sohan",56)# here that construtor will be called whose parameter matches. 
+
+# Class is the collection of dat(attribute) and methods(like functions)
