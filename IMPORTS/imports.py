c534bd61a6a9ca122a0fc57e2236de181d4f5709
Tejas12malviya <91682736+Tejas12malviya@users.noreply.github.com>
2025-01-06 12:36:06 +0530

    Update imports.py

--- IMPORTS/imports.py
+++ IMPORTS/imports.py
@@ -5,3 +5,10 @@ def percentage(marks):
         sum+= i
     final_score=round(float(sum/len(marks)),2)
     print(final_score)
+
+import random
+list=['Banana','car','Pepsico']
+print(random.choice(list))
+
+x=random.randint(1,10)
+print(x)
